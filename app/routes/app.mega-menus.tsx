4cc015eeede55cacf526554bd8fb852ebd77d4f2
Hüseyin Çetinkaya <huseyinctnkaya12@gmail.com>
2026-02-06 21:46:09 +0300

    update

--- app/routes/app.mega-menus.tsx
+++ app/routes/app.mega-menus.tsx
@@ -623,6 +623,13 @@ export default function MegaMenusList() {
                         >
                           <Copy className="w-4 h-4" />
                         </CustomButton>
+                        <CustomButton
+                          variant={"ghost" as any}
+                          size="sm"
+                          onClick={() => handleExport(menu)}
+                        >
+                          <Download className="w-4 h-4" />
+                        </CustomButton>
                         <div className="relative">
                           <CustomButton
                             variant={"ghost" as any}
